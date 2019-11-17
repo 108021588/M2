c6a8496aa955e257a61a7736fcd48c3a00adb82e
108021588 <108021588@live.asia.edu.tw>
2019-11-17 13:58:24 +0800

    M2Q14

--- src/Q14.java
+++ src/Q14.java
@@ -5,10 +5,9 @@ public class Q14 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n =scn.nextInt();
-        int sum=0,m;
+        int sum=0;
         for (int i=1;i<=n;i++){
-          m=i+1;
-          sum+=i*m;
+          sum+=i*(i+1);
         }
         System.out.println(sum);
     }
