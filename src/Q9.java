1fdf9cb396d277f557649c307154a946d4a0e44a
108021588 <108021588@live.asia.edu.tw>
2019-11-21 00:13:19 +0800

    M2Q9

--- src/Q9.java
+++ src/Q9.java
@@ -7,7 +7,6 @@ public class Q9 {
         int n =scn.nextInt();
         int m =scn.nextInt();
         int s=0,b=0;
-
         for (int i=1;i<=n;i++){
             if (n%i==0 && m%i==0){
                 s =i;
